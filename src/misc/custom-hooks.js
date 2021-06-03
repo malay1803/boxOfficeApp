658b61210436ef93f4970a1860f6fae5bd540855
malay1803 <18acharyamalay@gmail.com>
2021-06-03 22:54:49 +0530

    used custom hooks for starred shows

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,34 +1,32 @@
+import { useReducer, useEffect } from 'react';
 
-import { useReducer, useEffect } from 'react'
-
-function showsReducer(prevState, action){
-    switch(action.type){
-        case 'ADD':{
-            return [...prevState, action.showID]
-        }
-
-        case 'Remove': {
-            return prevState.filter( (showID) =>showID!==action.showID )
-        }
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
     }
 
-    default: return prevState
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+    default:
+      return prevState;
+  }
 }
 
-function usePersistedReducer (reducer, initialState, key){
-    const [state, dispatch] = useReducer(reducer, initialState, (initial)=>{
-        const persisted = localStorage.getItem(key);
-        return persisted ? JSON.parse(persisted) : initial;
-    })
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
 
-    useEffect(()=>{
-        localStorage.setItem(key, JSON.stringify(state))
-    }, [state, key])
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
 
-    return [state, dispatch]
-
+  return [state, dispatch];
 }
 
-export function useSHows(key = 'shows'){
-    return usePersistedReducer(showsReducer, [], key)
+export function useShows(key = 'shows') {
+  return usePersistedReducer(showsReducer, [], key);
 }
