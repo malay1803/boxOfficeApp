9bd829037a701072db2343235ceb20b6fa84e6b4
malay1803 <18acharyamalay@gmail.com>
2021-06-03 19:56:12 +0530

    created custom hood for show

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,34 @@
+
+import { useReducer, useEffect } from 'react'
+
+function showsReducer(prevState, action){
+    switch(action.type){
+        case 'ADD':{
+            return [...prevState, action.showID]
+        }
+
+        case 'Remove': {
+            return prevState.filter( (showID) =>showID!==action.showID )
+        }
+    }
+
+    default: return prevState
+}
+
+function usePersistedReducer (reducer, initialState, key){
+    const [state, dispatch] = useReducer(reducer, initialState, (initial)=>{
+        const persisted = localStorage.getItem(key);
+        return persisted ? JSON.parse(persisted) : initial;
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(key, JSON.stringify(state))
+    }, [state, key])
+
+    return [state, dispatch]
+
+}
+
+export function useSHows(key = 'shows'){
+    return usePersistedReducer(showsReducer, [], key)
+}
