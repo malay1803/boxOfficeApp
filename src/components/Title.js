5e91b5610f8cfa68a1d5d367ed3e658372ec38bc
malay1803 <18acharyamalay@gmail.com>
2021-06-04 00:21:10 +0530

    defined theme and created a few styled components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { TitleWrapper } from './Title.styled';
 
-const Title = ({ title, subtitle }) =>(
-        <div>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
-        </div>
-    )
+const Title = ({ title, subtitle }) => (
+  <TitleWrapper>
+    <h1>{title}</h1>
+    <p>{subtitle}</p>
+  </TitleWrapper>
+);
 
-export default Title
+export default Title;
